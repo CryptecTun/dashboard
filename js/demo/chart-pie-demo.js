faa59b6b04db89284de85ca18fd8ff7d4ffc1c9f
CryptecTun <100510381+CryptecTun@users.noreply.github.com>
2023-07-19 10:44:26 +0200

    server100

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,7 +7,7 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Online", "Offline", "Abnormal"],
     datasets: [{
       data: [166,20,100],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
